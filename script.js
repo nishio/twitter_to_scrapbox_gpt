7476d7b93e69ce2ac17bfd196322652e1f419131
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-21 21:26:33 +0900

    ver.13

--- script.js
+++ script.js
@@ -1,98 +1,115 @@
-// 選択範囲のツイートを取得する関数
-function getSelectedTweets() {
-  const selectedRange = window.getSelection().getRangeAt(0);
-  const elements = selectedRange
-    .cloneContents()
-    .querySelectorAll(".css-901oao");
-  const tweets = [];
+// ==UserScript==
+// @name         Twitter to Scrapbox
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Export selected tweets to Scrapbox format
+// @author       You
+// @match        https://twitter.com/*
+// @grant        none
+// ==/UserScript==
 
-  for (let el of elements) {
-    const parent = el.closest('[data-testid="tweet"]');
-    if (parent && !tweets.includes(parent)) {
-      tweets.push(parent);
-    }
-  }
-  return tweets;
-}
+(function () {
+  "use strict";
 
-const ACCOUNT_SELECTOR = '[href^="/"]';
-const CONTENT_SELECTOR = "[lang]";
-const TWEET_LINK_SELECTOR = '[href*="/status/"]';
-const IMAGE_SELECTOR = ".css-9pa8cd";
-const LINK_CARD_SELECTOR = ".css-4rbku5";
-const LINK_TITLE_SELECTOR = ".css-901oao";
+  const ACCOUNT_SELECTOR = '[href^="/"]';
+  const CONTENT_SELECTOR = "[lang]";
+  const TWEET_LINK_SELECTOR = '[href*="/status/"]';
+  const IMAGE_SELECTOR = ".css-9pa8cd";
+  const LINK_CARD_SELECTOR = ".css-4rbku5";
+  const LINK_TITLE_SELECTOR = ".css-901oao";
 
-// 画像を抜き出し、フォーマットする関数
-function extractAndFormatImages(tweet) {
-  const imageElements = Array.from(
-    tweet.querySelectorAll(IMAGE_SELECTOR)
-  ).slice(1);
-  const imageUrls = imageElements
-    .map((img) => `[${img.getAttribute("src")}#.png]`)
-    .join(" ");
+  function extractAndFormatImages(tweet) {
+    const imageElements = Array.from(
+      tweet.querySelectorAll(IMAGE_SELECTOR)
+    ).slice(1);
+    const imageUrls = imageElements
+      .map((img) => `[${img.getAttribute("src")}#.png]`)
+      .join(" ");
 
-  return imageUrls;
-}
-
-// リンク先のタイトルを取得する関数
-function extractLinkTitle(tweet) {
-  const linkElement = tweet.querySelector(LINK_CARD_SELECTOR);
-  const linkTitleElement = linkElement
-    ? linkElement.querySelector(LINK_TITLE_SELECTOR)
-    : null;
-  const linkTitle = linkTitleElement ? linkTitleElement.innerText : "";
-
-  return linkTitle;
-}
-
-// ツイートから必要な情報を取得し、Scrapbox形式に整形する関数
-function formatTweets(tweets) {
-  return tweets
-    .map((tweet) => {
-      const account = tweet
-        .querySelector(ACCOUNT_SELECTOR)
-        .getAttribute("href")
-        .substring(1);
-      const content = tweet
-        .querySelector(CONTENT_SELECTOR)
-        .innerText.split("\n")
-        .map((line) => `> ${line}`)
-        .join("\n");
-
-      // tweetIdをアカウント名のリンクから抽出
-      const permalink = tweet.querySelector(TWEET_LINK_SELECTOR);
-      const tweetId = permalink.href.split("/status/")[1].split("?")[0];
-
-      const tweetUrl = `https://twitter.com/${account}/status/${tweetId}`;
-
-      // 画像とリンクを抜き出し、フォーマット
-      const imageUrls = extractAndFormatImages(tweet);
-      const linkTitle = extractLinkTitle(tweet);
-
-      return `>[${account} ${tweetUrl}]\n${content}${
-        linkTitle ? "\n> " + linkTitle : ""
-      }${imageUrls ? "\n" + imageUrls : ""}\n`;
-    })
-    .join("\n");
-}
-
-// クリップボードにテキストをコピーする関数
-async function copyToClipboard(text) {
-  try {
-    await navigator.clipboard.writeText(text);
-    console.log("Copied to clipboard:", text);
-  } catch (err) {
-    console.error("Failed to copy:", err);
-  }
-}
-
-(async () => {
-  const selectedTweets = getSelectedTweets();
-  if (selectedTweets.length === 0) {
-    console.warn("No tweets found in the selected range.");
-    return;
+    return imageUrls;
   }
 
-  const formattedText = formatTweets(selectedTweets);
-  await copyToClipboard(formattedText);
+  function extractLinkTitle(tweet) {
+    const linkElement = tweet.querySelector(LINK_CARD_SELECTOR);
+    const linkTitleElement = linkElement
+      ? linkElement.querySelector(LINK_TITLE_SELECTOR)
+      : null;
+    const linkTitle = linkTitleElement ? linkTitleElement.innerText : "";
+
+    return linkTitle;
+  }
+
+  function formatTweets(tweets) {
+    return tweets
+      .map((tweet) => {
+        const account = tweet
+          .querySelector(ACCOUNT_SELECTOR)
+          .getAttribute("href")
+          .substring(1);
+        const content = tweet
+          .querySelector(CONTENT_SELECTOR)
+          .innerText.split("\n")
+          .map((line) => `> ${line}`)
+          .join("\n");
+
+        const permalink = tweet.querySelector(TWEET_LINK_SELECTOR);
+        const tweetId = permalink.href.split("/status/")[1].split("?")[0];
+
+        const tweetUrl = `https://twitter.com/${account}/status/${tweetId}`;
+
+        const imageUrls = extractAndFormatImages(tweet);
+        const linkTitle = extractLinkTitle(tweet);
+
+        return `>[${account} ${tweetUrl}]\n${content}${
+          linkTitle ? "\n> " + linkTitle : ""
+        }${imageUrls ? "\n" + imageUrls : ""}\n`;
+      })
+      .join("\n");
+  }
+
+  function copyToClipboard(text) {
+    const el = document.createElement("textarea");
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  }
+
+  function onButtonClick() {
+    const selectedTweets = Array.from(
+      window
+        .getSelection()
+        .getRangeAt(0)
+        .cloneContents()
+        .querySelectorAll('[data-testid="tweet"]')
+    );
+    const formattedText = formatTweets(selectedTweets);
+    copyToClipboard(formattedText);
+  }
+
+  function addButton() {
+    const nav = document.querySelector("nav");
+    if (!nav) return;
+
+    const button = document.createElement("button");
+    button.textContent = "Export to Scrapbox";
+    button.style.position = "fixed";
+    button.style.top = "10px";
+    button.style.right = "10px";
+    button.style.zIndex = 1000;
+    button.style.backgroundColor = "#1da1f2";
+    button.style.border = "none";
+    button.style.borderRadius = "4px";
+    button.style.color = "white";
+    button.style.padding = "6px 12px";
+    button.style.fontSize = "14px";
+    button.style.cursor = "pointer";
+
+    button.addEventListener("click", onButtonClick);
+
+    document.body.appendChild(button);
+  }
+
+  addButton();
 })();
